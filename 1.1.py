9f89d743f75235fc890ef05d5b108f3cbe4e668e
Aaronzdfxfgn <113214947+Aaronzdfxfgn@users.noreply.github.com>
2023-07-22 13:53:21 +1200

    First
    This is the first official lesson for actually coding

---
+++ 1.1.py
@@ -0,0 +1,11 @@
+p = (4,5)
+x,y = p 
+print(x)
+
+
+hello = ['hi!',"whatsup",'howdy','sup','yo']
+alternative1, alter2, alter3, *colloquial_language = hello
+print(alternative1)
+print(alter2)
+print(alter3)
+print(colloquial_language)
